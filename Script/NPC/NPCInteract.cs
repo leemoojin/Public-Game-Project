f96c1a143f38866f2739d2503b0987acc2f8af9b
leemoojin <hzm_deli@naver.com>
2024-12-20 18:27:00 +0900

    [Update] NPC

--- Script/NPC/NPCInteract.cs
+++ Script/NPC/NPCInteract.cs
@@ -1,24 +1,57 @@
-﻿using UnityEngine;
+﻿using MBT;
+using System;
+using UnityEditor;
+using UnityEngine;
 
 namespace NPC
 {
     public class NPCInteract : MonoBehaviour, IInteractable
     {
+        [field: Header("References")]
+        [field: SerializeField] public NPCDataSO NPCData { get; private set; }
 
         // IInteractable
+        [field: Header("Interactable")]
         [field: SerializeField] public bool IsInteractable { get; set; }// true 일때만 상호작용가능
         [field: SerializeField] public float InteractHoldTime { get; set; } = 0f;// 1초 이상일때 홀드인터렉트 진행
 
-        // Player UI
-        [field: SerializeField] public string ObjectName { get; set; }// SO
-        [field: SerializeField] public string InteractKey { get; set; }// SO
-        [field: SerializeField] public string InteractType { get; set; }// SO
+        [field: Header("Interactable")]
+        [SerializeField] private string currentState;
 
+        // Player UI
+        public string ObjectName { get; set; }// SO
+        public string InteractKey { get; set; }// SO
+        public string InteractType { get; set; }// SO
+
+        //
+        public GameObject BT;
+        private Blackboard _blackboard;
+
+        private void Awake()
+        {
+            _blackboard = BT.GetComponent<Blackboard>();
+        }
+
+        private void Start()
+        {
+            ObjectName = NPCData.Data.NPCName;
+            InteractKey = NPCData.Data.InteractKey;
+            InteractType = NPCData.Data.InteractType;
+
+            //Debug.Log($"npc key, {_blackboard.variables[0].key}");          
+            //Debug.Log($"npc GetVariable, {_blackboard.GetVariable<Variable<bool>>(_blackboard.variables[0].key).Value}");
+
+            NPCState state = NPCState.Move | NPCState.Dead;
+            currentState = state.ToString();
+
+            Debug.Log($"state, {state}");          
+        }
 
         public void Interact()
         {
             Debug.Log("NPCInteract - NPC와 상호작용 성공");
-
+            IsInteractable = false;
+            _blackboard.GetVariable<Variable<bool>>("isFollow").Value = true;
 
             // 대화 스크립트를 출력하는 메서드 호출하면 됨
             // 1회성 대화일 경우 대화 후 IsInteractable 값을 false 로 변경할 것
