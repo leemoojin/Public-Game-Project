86dab3d22435f7fe6e4a72d6a24c65b62f821da6
leemoojin <hzm_deli@naver.com>
2024-12-27 19:26:11 +0900

    [Add] Player

---
+++ Script/Player/Sound/SoundData.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+[Serializable]
+public class SoundData
+{
+    public string tag;
+    public AudioClip[] noises;
+    public float volume;
+    public float pitch;
+    public float noiseAmount;
+}
+
